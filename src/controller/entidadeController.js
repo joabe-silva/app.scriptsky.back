1646a528b90697184f59d631402c32f7388d6945
Joabe Silva <joabesilva533@gmail.com>
2021-02-04 21:57:17 -0300

    criando funcoes cadastro-entidade, editar-entidades, entidade, entidades no controller da aplicacao

--- src/controller/entidadeController.js
+++ src/controller/entidadeController.js
@@ -2,20 +2,29 @@ const db = require("../config/databases");
 
 exports.entidadeController = {
   
-    async cadastroEntidadeFuncionario(req, res) {
+    async cadastroEntidade(req, res) {
 
-        console.log(req.body.nome, req.body.contato, req.body.email, req.body.senha, req.body.situacao, req.body.tipo)
+        const ent = db.query("INSERT INTO entidade(nome, contato, email, senha, situacao, tipo) values ('"+req.body.nome+"', '"+req.body.contato+"', '"+req.body.email+"', '"+req.body.senha+"', '"+req.body.situacao+"', '"+req.body.tipo+"');");
+        return res.json('Entidade cadastrada com sucesso!');
 
-        const result = await db.insertCustomer({nome: req.body.nome, contato: req.body.contato, email: req.body.email, senha: req.body.senha, situacao: req.body.situacao, tipo: req.body.tipo});
-        //db.query("INSERT INTO entidade(nome, contato, email, senha, situacao, tipo) values ("req.body.nome}", '(85)99999-9999', 'joabe@gmail.com', '123456', 0, 0);");
-        return res.json(result + ': Cliente cadastrada com sucesso!');
     },
-    async listaEntidades(req, res) {
-        const result = db.query(
-            "SELECT * FROM entidade"
-        );
+    async editarEntidade(req, res) {
 
-        return res.json('Entidade:'+(await result).rows.values);
+        const ent = db.query("UPDATE entidade SET nome ='"+req.body.nome+"', SET contato ='"+req.body.contato+"', SET email = '"+req.body.email+"', SET senha = '"+req.body.senha+"', SET situacao = '"+req.body.situacao+"', SET tipo='"+req.body.tipo+"' WHERE cod_entidade = "+req.params.cod+");");
+        return res.json('Dados alterados com sucesso!');
+
+    },
+    async entidade(req, res) {
+
+        const ent = await db.query("SELECT * FROM entidade WHERE cod_entidade="+req.params.cod+";");
+        return res.json(ent);
+
+    },
+    async entidades(req, res) {
+
+        const ent = await db.query("SELECT * FROM entidade");
+        return res.json(ent);
+
     }
   
 };
--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const routes = express.Router();
 
-const Init = require('../src/controller/Init');
+const init = require('../src/controller/Init');
 const entidadeController = require('./controller/entidadeController');
 
 //Cria todas as tabelas do banco de dados
-routes.post('/init-create-tables', Init.Init.createTables)
+routes.post('/init-create-tables', init.Init.createTables)
 //Deleta todas as tabelas do banco de dados
-routes.post('/init-drop-tables', Init.Init.dropTables)
+routes.post('/init-drop-tables', init.Init.dropTables)
 //Rotas comuns
-routes.post('/cadastro-funcionario', entidadeController.entidadeController.cadastroEntidadeFuncionario)
-routes.get('/lista-entidades', entidadeController.entidadeController.listaEntidades)
+routes.post('/cadastro-entidade', entidadeController.entidadeController.cadastroEntidade)
+routes.put('/editar-entidade/:cod', entidadeController.entidadeController.editarEntidade)
+routes.get('/entidade/:cod', entidadeController.entidadeController.entidade)
+routes.get('/entidades', entidadeController.entidadeController.entidades)
 
 /*
 routes.get('/item/:id', ItemController.item);
